1f3ef0095a8a27bb89f5026c6a1aa155599af993
Chris Timperley <christimperley@googlemail.com>
2016-12-16 14:36:12 -0500

    fixed entrypoint

--- turtlebot_entrypoint.sh
+++ turtlebot_entrypoint.sh
@@ -7,6 +7,6 @@ source "/opt/ros/${ROS_DISTRO}/setup.bash"
 # start up dbus and avahi daemons
 # TODO: check these aren't already running!
 dbus-daemon --system
-/etc/init.d/avahi start
+/etc/init.d/avahi-daemon start
 
 exec "$@"
